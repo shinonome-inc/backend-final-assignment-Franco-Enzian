ba1752baf99c27252b7b24a142a52386b0d66c21
4Franco-star <emf0911b@gmail.com>
2023-09-01 22:47:54 +0900

    CI

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,9 @@
 # from django.shortcuts import render
+from django.conf import settings
 from django.contrib.auth import authenticate, login
 from django.contrib.auth import views as auth_views
-from django.conf import settings
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, TemplateView
-
 
 from .forms import SignupForm
 

26f4608ebe903d98d55d7d1cd89ecd195a37404f
4Franco-star <emf0911b@gmail.com>
2023-12-06 16:07:19 +0900

    FriendShipモデル実装

--- accounts/models.py
+++ accounts/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import AbstractUser
+from django.core.exceptions import ValidationError
 from django.db import models
 
 
@@ -6,4 +7,17 @@ class User(AbstractUser):
     email = models.EmailField()
 
 
-# class FriendShip(models.Model):
+class FriendShip(models.Model):
+    following = models.ForeignKey(User, related_name="following", on_delete=models.CASCADE)
+    followed = models.ForeignKey(User, related_name="followed", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ("following", "followed")
+
+    def clean(self):
+        if self.following == self.followed:
+            raise ValidationError("自分自身をフォローすることはできません。")
+
+    def __str__(self):
+        return f"{self.following} follows {self.followed}"
